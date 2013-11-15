4720d804806a9ec24ccbaaf2c71c687d95f62edd
Yannick Valentin <yannick@yvalentin.com>
2013-11-15 14:42:36 +0100

    Ajout méthode toggle_play();

--- js/color_changer.js
+++ js/color_changer.js
@@ -12,6 +12,8 @@ function color_changer(config) {
 	// Variables
 	var colors = new Array;
 	var element = params.element;
+	var interval;
+	var is_playing = false;
 
 	if(params.number != '' && typeof params.number == 'number') {
 		for(i=1 ; i <= params.number; i++) {
@@ -21,10 +23,15 @@ function color_changer(config) {
 
 	this.play = function(){	
 
-		var p = 1;
+		var p = 2;
+
+		interval_timer = params.timer;
+		is_playing = true;
+
+		element.style.backgroundColor = colors[1];
 
 		// Affiche les autres images
-    	var interval = setInterval(function() { 
+    	interval = setInterval(function() { 
 
 						element.style.backgroundColor = colors[p];
 
@@ -38,25 +45,27 @@ function color_changer(config) {
 						if(p >= colors.length) 
 							clearInterval(interval);
 
-                	}, params.timer);
+                	}, interval_timer);
 	}
 
 	this.stop = function(){
 		element.removeAttribute('style');
+		clearInterval(interval);
+		is_playing = false;
 	}
 
 	this.toggle_play = function(){
-		if(!play())
-			play();
+		if(is_playing == false)
+			this.play();
 		else
-			alert('stop');
+			this.stop();
 	}
 }
 
 var color_changer = new color_changer({
 		element: 		document.getElementById('color_changer'),
 		number: 		10,
-		timer: 			1000,
+		timer: 			50,
 		repeat: 		true
 });
 //color_changer.method();
